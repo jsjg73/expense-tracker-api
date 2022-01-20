f93525fdd9635e3d745ecc42c29378ea16927dac
jsjg73 <kjs930703@naver.com>
2022-01-20 23:37:27 +0900

    service and repository for registerUser

--- expensetracker_db.sql
+++ expensetracker_db.sql
@@ -1,28 +1,28 @@
+drop table if exists et_transactions;
+drop table if exists et_categories;
 drop table if exists et_users;
 
 create table et_users(
-user_id int AUTO_INCREMENT primary key not null,
+user_id MEDIUMINT AUTO_INCREMENT primary key not null,
 first_name varchar(20) not null,
 last_name varchar(20) not null,
 email varchar(50) not null,
 password text not null
 );
 
-drop table if exists et_categories;
 create table et_categories(
-category_id int AUTO_INCREMENT primary key not null,
-user_id int not null,
+category_id MEDIUMINT AUTO_INCREMENT primary key not null,
+user_id MEDIUMINT not null,
 title varchar(20) not null,
 description varchar(50) not null,
 constraint cat_users_fk foreign key (user_id)
     references et_users(user_id)
 );
 
-drop table if exists et_transactions;
 create table et_transactions(
-transaction_id int AUTO_INCREMENT primary key not null,
-category_id int not null,
-user_id int not null,
+transaction_id MEDIUMINT AUTO_INCREMENT primary key not null,
+category_id MEDIUMINT not null,
+user_id MEDIUMINT not null,
 amount decimal(10,2) not null,
 note varchar(50) not null,
 transaction_date bigint not null,
